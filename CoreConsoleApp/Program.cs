58be2bebc455c5f7426a93f44dfa8cafdd7bb690
Brandon Zuech <brandonzuech@hotmail.com>
2018-03-22 16:12:59 -0400

    update

--- CoreConsoleApp/Program.cs
+++ CoreConsoleApp/Program.cs
@@ -15,22 +15,22 @@ namespace CoreConsoleApp
             {
                 var authsome = new AuthsomeService();
 
-                var result = await authsome.GetAsync<object>("https://orbose.ngrok.io/test",
+                var result = await authsome.GetAsync<BingJson_Rootobject>("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US",
                     (header) =>
                     {
                         header.IncludeUserAgent("Happy Fun User");
                         header.IncludeAcceptMediaType(MediaType.application_json);
                     });
 
-                //if (result.httpStatusCode == System.Net.HttpStatusCode.OK)
-                //{
-                //    var image = result.Content.images.FirstOrDefault();
-                //    Console.WriteLine("image url" + image.url);
-                //}
-                //else
-                //{
-                //    Console.WriteLine(result.httpStatusCode.ToString() + " - " + result.ErrorJson);
-                //}
+                if (result.httpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var image = result.Content.images.FirstOrDefault();
+                    Console.WriteLine("image url" + image.url);
+                }
+                else
+                {
+                    Console.WriteLine(result.httpStatusCode.ToString() + " - " + result.ErrorJson);
+                }
                 Console.ReadLine();
 
             }).Wait();
